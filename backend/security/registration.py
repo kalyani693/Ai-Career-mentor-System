1dcb446fabc050cb0e77e2d8e553aca2dc336493
kalyani kapade <kalyanikapade6@gmail.com>
2026-07-12 00:22:22 +0530

    deleteaccount and renewaccount endponits are added

--- backend/security/registration.py
+++ backend/security/registration.py
@@ -1,10 +1,11 @@
-from fastapi import HTTPException,Depends
+from fastapi import HTTPException,Depends,status
 from database.schemas import getdb,registered_users,registered_admin
 import database.schemas as model
 from sqlalchemy.orm import session
 from pwdlib import PasswordHash
 from typing import Annotated
 from jose import jwt
+from sqlalchemy import text
 import os
 from dotenv import load_dotenv
 from fastapi.security import OAuth2PasswordBearer
@@ -16,7 +17,8 @@ load_dotenv()
 password_hash=PasswordHash.recommended()
 dependancy=Annotated[session,Depends(getdb)]
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
-adminoauth2_scheme = OAuth2PasswordBearer(tokenUrl="/adminLogin")
+adminoauth2_scheme = OAuth2PasswordBearer(tokenUrl="/admin-pannel/adminLogin")#iska path isliye change hua 
+#kyuki ab hamara admin dashboard /admin-pannel/docs is server pr hai
 
 
 # add modular coding
@@ -33,7 +35,8 @@ class authentication():
     if user:
         raise HTTPException(status_code=400,detail="Account with this Username is Already Present")
     if email:
-        raise HTTPException(status_code=400,detail="Account with This Email Id Is already Present")
+        raise HTTPException(status_code=400,detail="""Account with This Email Id Is already Present.
+                             If you want to renew your existing account with same email you are able to do that from renewaccount tab.""")
     hashpassword=password_hash.hash(info.Password)
     try:
         #store user information into database
@@ -63,6 +66,8 @@ class authentication():
     #add->only 3 times retry otherwise stop login for 10 min
   try:  
     user=db.query(registered_users).filter(registered_users.Username==credential.username).first()
+    if user.is_active==False:
+       raise HTTPException( status_code=406,detail=f"Account with this Username has been deleted. status:Not active")
     if user:
         if password_hash.verify(credential.password,user.hashed_Password):
             secret_key=os.getenv('SECRET_KEY')
@@ -75,8 +80,42 @@ class authentication():
         raise HTTPException(status_code=400,detail="Account with this Username is not Available")
   except Exception as e:
       raise HTTPException(status_code=500, detail={"error":str(e)})  
-    
 
+ async def deleteaccount(self,user,db):
+    try: 
+        query=text(f"""update registered_users set is_active=False where "Username"='{user.Username}'; """)
+        response=db.execute(query)
+        db.commit()
+        if response._soft_closed==True:
+           return {"response":"Your account is deleted successfully!!"} 
+        else:
+           return {"response":"sorry, something went wrong"}
+           
+    except Exception as e:
+       raise HTTPException(status_code=500,detail=f"error:{str(e)}")  
+
+ async def renewacc(self,info,db):
+   try:  
+    user=db.query(registered_users).filter(registered_users.Username==info.username).first()
+    if user.is_active==True:
+       raise HTTPException( status_code=406,detail=f"status:active, No need to Renew this aacount. you can directly login to access information.")
+    if user:
+        if password_hash.verify(info.password,user.hashed_Password):
+            query=text(f"""update registered_users set is_active=True where "Username"='{info.Username}'; """)
+            response=db.execute(query)
+            db.commit()
+            if response._soft_closed==True:
+             return {"response":"Your account is Renewed successfully!!"} 
+            else:
+              return {"response":"sorry, something went wrong"}
+        else:
+            raise HTTPException(status_code=422,detail="password is wrong")
+    else:
+        raise HTTPException(status_code=400,detail="Account with this Username is not Available")
+   except Exception as e:
+      raise HTTPException(status_code=500, detail={"error":str(e)}) 
+    
+    
 async def check_user(db:dependancy, token:str=Depends(oauth2_scheme)):
     try:
         secret_key=os.getenv('SECRET_KEY')
@@ -106,7 +145,8 @@ class adminAuthentication():
     if admin:
         raise HTTPException(status_code=400,detail="Account with this Username is Already Present")
     if email:
-        raise HTTPException(status_code=400,detail="Account with This Email Id Is already Present")
+        raise HTTPException(status_code=400,detail="""Account with This Email Id Is already Present.
+                             If you want to renew your existing account with same email you are able to do that from renewaccount tab.""")
     hashpassword=password_hash.hash(info.Password)
     try:
         #stores admin information into database
@@ -122,6 +162,8 @@ class adminAuthentication():
     #add->only 3 times retry otherwise stop login for 10 min
   try:  
     admin=db.query(registered_admin).filter(registered_admin.Username==credential.username).first()
+    if admin.is_active==False:
+       raise HTTPException( status_code=406,detail=f"Account with this Username has been deleted. status:Not active")
     if admin:
         if password_hash.verify(credential.password,admin.hashed_Password):
             secret_key=os.getenv('SECRET_KEY')
@@ -133,7 +175,41 @@ class adminAuthentication():
     else:
         raise HTTPException(status_code=400,detail="Account with this Username is not Available")
   except Exception as e:
-      raise HTTPException(status_code=500, detail={"error":str(e)})  
+      raise HTTPException(status_code=500, detail={"error":str(e)})
+   
+ async def deleteaccount(self,admin,db):
+    try:
+        query=text(f"""update registered_admin set is_active=False where "Username"='{admin.Username}'; """)
+        response=db.execute(query)
+        db.commit()
+        if response._soft_closed==True:
+           return {"response":"Your account is deleted successfully!!"} 
+        else:
+           return {"response":"sorry, something went wrong"}
+    except Exception as e:
+        raise HTTPException(status_code=500,detail=f"error:{str(e)}")  
+ 
+ async def renewacc(self,info,db):
+   try:  
+    admin=db.query(registered_admin).filter(registered_admin.Username==info.username).first()
+    if admin.is_active==True:
+       raise HTTPException( status_code=406,detail=f"status:active, No need to Renew this aacount. you can directly login to access information.")
+    if admin:
+        if password_hash.verify(info.password,admin.hashed_Password):
+            query=text(f"""update registered_admin set is_active=True where "Username"='{info.Username}'; """)
+            response=db.execute(query)
+            db.commit()
+            if response._soft_closed==True:
+              return {"response":"Your account is Renewed successfully!!"} 
+            else:
+              return {"response":"sorry, something went wrong"}
+        else:
+            raise HTTPException(status_code=422,detail="password is wrong")
+    else:
+        raise HTTPException(status_code=400,detail="Account with this Username is not Available")
+   except Exception as e:
+      raise HTTPException(status_code=500, detail={"error":str(e)})
+
 
 async def check_admin(db:dependancy, token:str=Depends(adminoauth2_scheme)):
     try:
