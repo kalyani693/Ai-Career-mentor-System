64e525cbb8fb0d2ff6149ea11356070e093996b4
kalyani kapade <kalyanikapade6@gmail.com>
2026-09-25 14:32:20 +0530

    .

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app_admin=FastAPI(title="Admin Dashboard")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://aicareermentorfrontend.vercel.app"],  
+    allow_origins=["https://aicareermentorfrontend.vercel.app","http://localhost:5173"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
